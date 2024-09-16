0bcc46887e313cf73091514dddb714246ce37a89
Jeyker-Dev <134793631+J0K3R1178@users.noreply.github.com>
2024-09-16 11:35:13 -0400

    Agregados Estilos Para Computadora

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  presets: [
+    require('./src/lib/mypreset.js')
+  ],
   theme: {
+    fontFamily: {
+      vesper: ["Vesper Libre", "serif"],
+    },
     extend: {},
   },
   plugins: [],
